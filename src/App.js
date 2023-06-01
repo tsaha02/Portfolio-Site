0a1e79de3daddf78269596142e5d4f4453ace008
Tanmoy Saha <110479171+tsaha02@users.noreply.github.com>
2023-06-01 20:09:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <Router>
         <NavigationBar />
         <Routes>
+    <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
